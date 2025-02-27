c8932d9120f830d9888f04cb9b8b0bc5c13545e5
gcsusy <72149143+gcsusy@users.noreply.github.com>
2025-02-26 21:25:23 -0600

    update cors

---
+++ src/main/java/gc/unir/actividad2/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package gc.unir.actividad2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://actividad1-gctiendav1.vercel.app/","http://localhost:5173","https://personal-search-6042288385.us-east-1.bonsaisearch.net:443")); // Permite tu frontend
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Content-Type", "Authorization"));
+        //config.setAllowCredentials(true); // Si usas cookies o autenticación
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
